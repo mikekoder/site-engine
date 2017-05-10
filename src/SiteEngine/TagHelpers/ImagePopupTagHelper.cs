2377315345d75d3c64b2346a48d71f8db66068ef
Mika Kolari <mika.kolari@soikea.com>
2017-05-10 20:10:57 +0300

    ImagePopupTagHelper

---
+++ src/SiteEngine/TagHelpers/ImagePopupTagHelper.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteEngine.TagHelpers
+{
+    [HtmlTargetElement("img", Attributes = "popup")]
+    public class ImagePopupTagHelper : TagHelper
+    {
+        public int? Width { get; set; }
+        public int? Height { get; set; }
+        public string Src { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var id = Guid.NewGuid().ToString();
+
+            output.TagName = "div";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            output.Attributes.Clear();
+            output.Content.SetHtmlContent($@"<a href=""#"" data-toggle=""modal"" data-target=""#{id}""><img src=""{Src}"" width=""{Width}"" height=""{Height}"" /></a>
+
+<div id=""{id}"" class=""modal fade"" tabindex=""-1"" role=""dialog"">
+  <div class=""modal-dialog"">
+    <div class=""modal-content"">
+        <div class=""modal-body"">
+            <img src=""{Src}"" class=""img-responsive"">
+        </div>
+    </div>
+  </div>");
+        }
+    }
+}
