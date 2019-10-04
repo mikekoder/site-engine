f01794d5d9df040e3a941d531bff6b4aefd1ad21
Mike Koder <mkoderfi@gmail.com>
2019-10-04 20:25:56 +0300

    Html snippet tag helper

---
+++ src/SiteEngine/TagHelpers/HtmlSnippetTagHelper.cs
@@ -0,0 +1,25 @@
+﻿using HeyRed.MarkdownSharp;
+using HtmlAgilityPack;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace SiteEngine.TagHelpers
+{
+    [HtmlTargetElement("html-snippet")]
+    public class HtmlSideBySideTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "row");
+            var inner = output.GetChildContentAsync().Result;
+            var content = inner.GetContent();
+            var html = $@"<div class=""col s6"">{HttpUtility.HtmlEncode(content).Replace("\n", "<br />")}</div><div class=""col s6"">{content.Replace("\n","<br />")}</div>";
+            output.Content.SetHtmlContent(html);
+        }
+    }
+}
